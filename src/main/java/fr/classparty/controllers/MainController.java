1a236885f1a0a6dd5c6683862279b8921501b825
DDaFox <DaFox45200@gmail.com>
2016-01-26 09:45:31 +0100

    Création d'une jsp générique pour manipuler le controller.

--- src/main/java/fr/classparty/controllers/MainController.java
+++ src/main/java/fr/classparty/controllers/MainController.java
@@ -1,8 +1,21 @@
 package fr.classparty.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
 public class MainController {
 
-	public static void main(String[] args) {
-		System.out.println("Ceci est le main.");
+	
+	@RequestMapping("/login.jsp")
+	public ModelAndView hello(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		ModelAndView model = new ModelAndView("login");
+		model.addObject("pseudo", "Antoine");
+		return model;
 	}
 }
