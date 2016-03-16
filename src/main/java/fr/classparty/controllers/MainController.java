53074a8c4df3660dd0c78a0767b032685ce7925f
RémyTong <remy.tong@epsi.fr>
2016-03-16 14:13:32 +0100

    MODIFICATION DU FICHIER XML POUR AFFICHER LES VUES

--- src/main/java/fr/classparty/controllers/MainController.java
+++ src/main/java/fr/classparty/controllers/MainController.java
@@ -4,18 +4,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
 @Controller
+@RequestMapping("/login")
+
 public class MainController {
 
-	
-	@RequestMapping("/login.jsp")
-	public ModelAndView hello(HttpServletRequest request,
+	/*public ModelAndView hello(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 
 		ModelAndView model = new ModelAndView("login");
 		model.addObject("pseudo", "Antoine");
 		return model;
-	}
+	}*/
+	
+
+	
+	@RequestMapping(method = RequestMethod.GET)
+	   public String printHello(ModelMap model) {
+	      model.addAttribute("message", "Hello Spring MVC Framework!");
+	      return "login";
+	   }
+	
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -1,18 +1,16 @@
-<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
-		<spring:url value="/resources/css/main.css" var="mainCss" />
-		<spring:url value="/resources/js/jquery.1.10.2.min.js" var="jqueryJs" />
-		<spring:url value="/resources/js/main.js" var="mainJs" />
 		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 		<title>Insert title here</title>
 	</head>
 	<body>
 	
-		Vous êtes connecté en tant que ${pseudo}
+		Vous êtes connecté en tant que ${message}
+		<%@ include file="essaie.jsp"%>	
 	
 	</body>
 </html>
